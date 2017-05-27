19760c31e27cebb406d5c9de59dbeda97f20a9b5
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-05-27 14:30:22 +0200

    Should do complete local build, will add that later

--- build.ps1
+++
@@ -1,8 +0,0 @@
-$ImageName="profilebot"
-
-function Invoke-Docker-Build ([string]$ImageName, [string]$ImagePath, [string]$DockerBuildArgs = "") {
-    echo "docker build -t $ImageName $ImagePath $DockerBuildArgs"
-    Invoke-Expression "docker build -t $ImageName $ImagePath $DockerBuildArgs"
-}
-
-Invoke-Docker-Build -ImageName $ImageName -ImagePath "."
