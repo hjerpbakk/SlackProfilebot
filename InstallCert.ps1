6ae6331875ec05b892dc0a8cf7cbca51b4d42465
Runar Ovesen Hjerpbakk <runar@hjerpbakk.com>
2017-07-06 20:34:20 +0200

    Does it work

--- InstallCert.ps1
+++ InstallCert.ps1
@@ -2,7 +2,7 @@ $invocation = (Get-Variable MyInvocation).Value
 $directorypath = Split-Path $invocation.MyCommand.Path
 $certPath = $directorypath + "\profilebot.cer"
 $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certPath)
-$rootStore = Get-Item cert:\CurrentUser\Root
+$rootStore = Get-Item cert:\LocalMachine\Root
 $rootStore.Open("ReadWrite")
 $rootStore.Add($cert)
 $rootStore.Close()
